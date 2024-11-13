263749308e24ea246a67cf56c01536b2e00a54a7
Parth <parthmungra1372@gmail.com>
2024-11-13 13:03:48 +0530

    Ger All Study Material Tjhat uploaded

--- client/src/components/faculty/getAllStudyMaterial/getAllStudyMaterial.js
+++ client/src/components/faculty/getAllStudyMaterial/getAllStudyMaterial.js
@@ -3,7 +3,7 @@ import Body from "./Body";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
 
-const AddStudyMaterial = () => {
+const GetAllStudyMaterial = () => {
   return (
     <div className="bg-[#000000] h-screen flex items-center justify-center overflow-hidden">
       <div className="flex flex-col bg-[#000000] h-5/6 w-[95%] rounded-2xl shadow-2xl space-y-6 overflow-auto">
@@ -17,4 +17,4 @@ const AddStudyMaterial = () => {
   );
 };
 
-export default AddStudyMaterial;
+export default GetAllStudyMaterial;
