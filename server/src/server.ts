891f310ddc1d55f48ba8f49cabc6cec48c24b2ab
Parth <parthmungra1372@gmail.com>
2024-11-13 12:54:07 +0530

    Cors Handled

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,14 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 // Enable CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://universitymanagementsystem.vercel.app', // Allow Vercel frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    credentials: true // Allow cookies to be sent if needed
+  })
+);
+
 
 // Routes
 app.use("/api/admin", adminRoutes);
